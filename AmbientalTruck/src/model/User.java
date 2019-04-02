20e5ee1f70694cc2a12039a02c5a0bee32a9aa21
Pc <manuellpsk@gmail.com>
2019-04-02 12:08:34 -0300

    Inicio del proyecto Login

---
+++ AmbientalTruck/src/model/User.java
@@ -0,0 +1,38 @@
+package model;
+public class User {
+
+    private String id;
+    private String password;
+    private int tipo;
+    
+    public User(String id, String password, int tipo){
+        this.id=id;
+        this.password=password;
+        this.tipo=tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+}
